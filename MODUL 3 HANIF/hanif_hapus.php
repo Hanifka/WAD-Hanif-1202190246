92631346fa799b56c538f09da3a775de904e5ce7
jackzay <59630877+jackzay@users.noreply.github.com>
2021-11-24 14:27:12 +0700

    add bugs m3

--- MODUL 3 HANIF/hanif_hapus.php
+++ MODUL 3 HANIF/hanif_hapus.php
@@ -1,18 +1,18 @@
-<?php 
-// koneksi database
-include 'koneksi.php';
- 
-// menangkap data id yang di kirim dari url
-$id_buku = $_GET['id_buku'];
- 
- 
-// menghapus data dari database
-mysqli_query($koneksi,"delete from buku_table where id_buku ='$id_buku'");
- 
-// mengalihkan halaman kembali ke index.php
-header("location:hanif_home.php");
- 
-?>
+    <?php 
+    // koneksi database
+    include 'koneksi.php';
+    
+    // menangkap data id yang di kirim dari url
+    $id_buku = $_GET['id_buku'];
+    
+    
+    // menghapus data dari database
+    mysqli_query($koneksi,"delete from buku_table where id_buku ='$id_buku");
+    
+    // mengalihkan halaman kembali ke index.php
+    header("location:hanif_home.php");
+    
+    ?>
 
 
 
